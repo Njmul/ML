b13095d24fd264e52258ba120df962809b827e45
Najmul <99996615+Njmul@users.noreply.github.com>
2025-12-18 02:50:55 +0900

    Add files via upload

---
+++ python/Day04/the_slicing_surgeon.py
@@ -0,0 +1,5 @@
+data = list(range(10,60,10))
+print(data)
+
+reverse_list = data[::-1] # -1: This is the step value. A step of -1 tells Python to traverse the list backward, one element at a time [1]. 
+print(reverse_list[-3:]) # showing value from -3 index to -1 (along with) 
