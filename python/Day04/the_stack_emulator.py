9939566d66e9c798e2763f28b1194d5d4bb8442c
Najmul <99996615+Njmul@users.noreply.github.com>
2025-12-18 02:52:48 +0900

    Add files via upload

---
+++ python/Day04/the_stack_emulator.py
@@ -0,0 +1,12 @@
+list = []
+print(list)
+
+# adding 1, 2, 3 in the list
+list.append(1)
+list.append(2)
+list.append(3)
+print(list)
+
+# remove last number of the list
+list.pop(-1) # index of last number is -1
+print(list)
