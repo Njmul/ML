aed621dc99779c39f29ad924a5897eb65bd310f4
Najmul <99996615+Njmul@users.noreply.github.com>
2025-12-20 12:21:47 +0900

    Add files via upload

---
+++ python/Day06/the_pure_return.py
@@ -0,0 +1,10 @@
+
+def add(a,b):
+    
+    sum = a+b
+    print(sum)      # It will not print anything
+
+    return      # Now return will be None. if return 0, there will be no output, outside of the function.
+
+res = add(2,3)
+print(res)
